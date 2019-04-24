3d37a548bedbaacb8f6b5776d05b9097f58d6b78
Eli Knebel <eliknebel@cmu.edu>
2019-04-23 23:03:00 -0400

    test has

--- trie-test.py
+++ trie-test.py
@@ -20,6 +20,18 @@ class TestImmutableTrie(unittest.TestCase):
 
     self.assertEqual(set(trie4.keys()), set(['key1', 'key2', 'abc']))
     self.assertEqual(set(trie4.values()), set(['value1', 'value2', '123']))
+
+  def test_has(self):
+    trie1 = ImmutableTrie()
+    trie2 = trie1.set('key1', 'value1')
+    trie3 = trie2.set('key2', 'value2')
+    trie4 = trie3.set('abc', '123')
+
+    self.assertTrue(trie4.has('key1'))
+    self.assertTrue(trie4.has('abc'))
+    self.assertFalse(trie4.has('k'))
+    self.assertFalse(trie4.has('ab'))
+    self.assertFalse(trie4.has('key123'))
     
 if __name__ == '__main__':
   unittest.main()
